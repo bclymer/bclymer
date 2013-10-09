c98dfb05f6e8891be7834548617cddfe6dde69ba
Brian Clymer <crdnilfan@gmail.com>
2013-10-08 21:29:55 -0500

    clock updater fix

--- bclymer/bclymer.go
+++ bclymer/bclymer.go
@@ -1,12 +1,18 @@
 package bclymer
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "bclymer/static/index.html")
+}
+
+func helpTime(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, time.Now().Unix())
 }
 
 func StartServer(urlPrefix string) {
@@ -14,6 +20,7 @@ func StartServer(urlPrefix string) {
 		urlPrefix = "/" + urlPrefix
 	}
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/currentTime.php", helpTime)
 
 	http.Handle(urlPrefix+"/static/", http.StripPrefix(urlPrefix+"/static", http.FileServer(http.Dir("bclymer/static"))))
 	log.Println("bClymer is running...")
