988e0afb732e8207f1886a7570e5ca925ea65c44
Brian Clymer <crdnilfan@gmail.com>
2013-11-08 08:54:11 -0600

    Reorganize

--- bclymer.go
+++ bclymer.go
@@ -1,23 +1,41 @@
 package main
 
 import (
-	"bclymer/aboutme"
-	"bclymer/bclymer"
-	"bclymer/quarto"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-func main() {
-	bclymer.StartServer()
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
 
-	qRedis := quarto.StartServer("quarto")
-	defer qRedis.Quit()
+func helpTime(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, time.Now().Unix())
+}
 
-	aboutme.StartServer()
-
-	err := http.ListenAndServe(":80", nil)
+func reportIP(w http.ResponseWriter, r *http.Request) {
+	file, err := os.OpenFile("rasp.txt", os.O_RDWR|os.O_APPEND, 0660)
+	defer file.Close()
 	if err != nil {
-		log.Print("ListenAndServe:", err)
+		return
 	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	file.WriteString(string(body))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/currentTime.php", helpTime)
+	http.HandleFunc("/reportip", reportIP)
+
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	log.Println("bClymer is running...")
+	http.ListenAndServe(":42125", nil)
 }
